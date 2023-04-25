9f4d9f0c10482b90d2ed59a1f65b4516aef60c31
andres0818 <c.andres.perez.h@gmail.com>
2023-04-25 13:49:43 -0500

    styles whit taildwind css

--- src/components/NavIcon.jsx
+++ src/components/NavIcon.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Switch, Text, View } from "react-native";
 
 const NavIcon = ({ ...props }) => {
   return (
-    <View style={styles.container}>
+    <View className="w-full flex-row justify-center ">
+      <Switch className='absolute left-0 top-4'
+        value={props.colorScheme === "dark"}
+        onChange={props.toggleColorScheme}
+      />
       <Image style={styles.logo} source={props.icon} />
     </View>
   );
@@ -12,12 +16,7 @@ const NavIcon = ({ ...props }) => {
 export default NavIcon;
 
 const styles = StyleSheet.create({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-  },
+
   logo: {
     width: "35%",
     resizeMode: "contain",
