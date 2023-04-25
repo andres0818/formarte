3e38af5e5d80ca33b9c7c3456f5ee764cf055c26
andres0818 <c.andres.perez.h@gmail.com>
2023-04-25 13:50:18 -0500

    theme dark and light

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Button } from "react-native";
 import Home from "./src/screens/Home/Home";
+import { useColorScheme } from "nativewind";
 import NavIcon from "./src/components/NavIcon";
-import icon from './src/img/formarte.png'
+import icon from "./src/img/formarte.png";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -16,7 +17,16 @@ export default function App() {
           name="Formarte"
           component={Home}
           options={{
-            headerTitle: () => <NavIcon  icon={icon}/>,
+            headerTitle: () => (
+              <NavIcon
+                colorScheme={colorScheme}
+                toggleColorScheme={toggleColorScheme}
+                icon={icon}
+              />
+            ),
+            headerStyle: {
+              backgroundColor: colorScheme === 'dark' ? "#6B7280" : "#FFFFFF",
+            },
           }}
         />
       </Stack.Navigator>
