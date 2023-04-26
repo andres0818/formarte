87b16b942d1a168ce90f48a8dc358f06609d8b49
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 12:25:10 -0500

    img navbar

---
+++ src/img/index.js
@@ -0,0 +1,8 @@
+import usuario from "./usuario.png";
+import prueba from "./prueba.png";
+import acceso from "./acceso.png";
+import entrenador from "./entrenador.png";
+import graduado from "./graduado.png";
+import home from "./home.png";
+
+export  { usuario, acceso, entrenador, graduado, home, prueba };
