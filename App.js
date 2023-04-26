48854bbb8612c6ce962ed7b450ace8e2714d434f
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 16:48:41 -0500

    add page of login and register

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./src/screens/Home/Home";
 import { useColorScheme } from "nativewind";
 import NavIcon from "./src/components/NavIcon";
 import icon from "./src/img/formarte.png";
+import { colorStyle } from "./src/Styles/variants";
+import Home from "./src/screens/Home";
+import Login from "./src/screens/Login";
+import { StatusBar } from "react-native";
+import Register from "./src/screens/Register";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   const { colorScheme, toggleColorScheme } = useColorScheme();
   return (
-    <NavigationContainer>
+    <NavigationContainer >
       <Stack.Navigator>
         <Stack.Screen
-          name="Formarte"
+          name="home"
           component={Home}
           options={{
             headerTitle: () => (
@@ -25,11 +29,47 @@ export default function App() {
               />
             ),
             headerStyle: {
-              backgroundColor: colorScheme === 'dark' ? "#6B7280" : "#FFFFFF",
+              backgroundColor:
+                colorScheme === "dark" ? colorStyle.dark : colorStyle.light,
             },
           }}
         />
+        <Stack.Screen
+          name="login"
+          component={Login}
+          options={{
+            headerStyle: {
+              backgroundColor:
+                colorScheme === "dark" ? colorStyle.dark : colorStyle.light,
+            },
+            headerLeft: () => (
+              <NavIcon
+                colorScheme={colorScheme}
+                toggleColorScheme={toggleColorScheme}
+                icon={icon}
+              />
+            ),
+          }}
+        />
+          <Stack.Screen
+          name="register"
+          component={Register}
+          options={{
+            headerStyle: {
+              backgroundColor:
+                colorScheme === "dark" ? colorStyle.dark : colorStyle.light,
+            },
+            headerLeft: () => (
+              <NavIcon
+                colorScheme={colorScheme}
+                toggleColorScheme={toggleColorScheme}
+                icon={icon}
+              />
+            ),
+          }}
+        />
       </Stack.Navigator>
+      <StatusBar hidden={true} />
     </NavigationContainer>
   );
 }
