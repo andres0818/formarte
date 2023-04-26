0811a313d9059078bd7da19a3a198db9adf44a36
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 11:08:50 -0500

    update props for component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 import fondo from "../img/bannerhome-3.png";
+import lineaHeader from "../img/lineaHeader.png";
 
-const Header = () => {
+const Header = ({
+  titulo = "Formarte Medellín",
+  subtitulo = "Preicfer y Preuniversitarios en Medellín",
+}) => {
   return (
-    <View className="w-full items-center">
-      <Text className="text-green-400 text-2xl font-bold m-3 mt-2">
-        Formarte Medellín
+    <View className="w-full items-center mb-8">
+      <Text className="text-green-400 text-2xl font-bold m-3 mt-8">
+        {titulo}
       </Text>
-      <Text className="text-slate-700 dark:text-white font-semibold text-lg m-4">
-        Preicfer y Preuniversitarios en Medellín
-      </Text>
-      <Text className="dark:text-white mb-8">
-        Filtra el programa que deseas estudiar
+      <Text className="text-slate-700 dark:text-white font-semibold text-lg mb-8">
+        {subtitulo}
       </Text>
       <Image className="w-full h-40 absolute" source={fondo} />
+      <Image className='w-64 h-1 ' source={lineaHeader}/>
     </View>
   );
 };
