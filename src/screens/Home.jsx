81558224efc2732498f7740be56030de56c45b67
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 15:13:04 -0500

    fixed bug

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
-import Header from "../../components/Header";
-import Filter from "../../components/Filter";
-import Slide from "../../components/Slide";
-import InfoText from "../../components/InfoText";
-import NavBar from "../../components/NavBar";
+import Header from "../components/Header";
+import Filter from "../components/Filter";
+import Slide from "../components/Slide";
+import InfoText from "../components/InfoText";
+import NavBar from "../components/NavBar";
 import { KeyboardAvoidingScrollView } from "@tbnb/react-native-keyboard-sticky-avoiding-scroll-view";
-import Footer from "../../components/Footer";
+import Footer from "../components/Footer";
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View className="flex-1 bg-white dark:bg-gray-700">
-      <KeyboardAvoidingScrollView stickyFooter={<NavBar />}>
+      <KeyboardAvoidingScrollView stickyFooter={<NavBar navigation={navigation}/>}>
         <Header />
         <View className="mb-10"></View>
         <Slide />
