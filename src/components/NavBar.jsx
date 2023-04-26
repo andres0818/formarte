4872d97aee85172ff6b40fa498abea6ea8a973be
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 14:07:37 -0500

    create component for navigation

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
+import * as icon from "../img";
+import { KeyboardAvoidingScrollView } from "@tbnb/react-native-keyboard-sticky-avoiding-scroll-view";
+
+const NavBar = () => {
+  return (
+      <View style={styles.container} className=" bg-white dark:bg-gray-500">
+        <TouchableOpacity>
+          <Image style={styles.img} source={icon.home} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.img} source={icon.entrenador} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.img} source={icon.prueba} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.img} source={icon.graduado} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image style={styles.img} source={icon.usuario} />
+        </TouchableOpacity>
+      </View>
+  );
+};
+
+export default NavBar;
+
+const styles = StyleSheet.create({
+  container: {
+    elevation: 20,
+    paddingHorizontal: 8,
+    paddingTop: 4,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  img: {
+    width: 54,
+    height: 54,
+  },
+});
