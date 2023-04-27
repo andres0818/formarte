80ae4a8cad4c6e96430aed2a4e70bf86c72dd16d
andres0818 <c.andres.perez.h@gmail.com>
2023-04-26 22:06:11 -0500

    focus screen

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,50 @@
 import React from "react";
 import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 import * as icon from "../img";
-import { KeyboardAvoidingScrollView } from "@tbnb/react-native-keyboard-sticky-avoiding-scroll-view";
+import { RUTAS } from "../RoutesVariants";
 
-const NavBar = ({navigation}) => {
+const NavBar = ({ navigation, route }) => {
   return (
-      <View style={styles.container} className=" bg-white dark:bg-gray-500">
-        <TouchableOpacity
-        onPress={()=>navigation.navigate('home')}>
-          <Image style={styles.img} source={icon.home} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image style={styles.img} source={icon.entrenador} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image style={styles.img} source={icon.prueba} />
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Image style={styles.img} source={icon.graduado} />
-        </TouchableOpacity>
-        <TouchableOpacity 
-        onPress={()=>navigation.navigate('login')}
-        >
-          <Image style={styles.img} source={icon.usuario} />
-        </TouchableOpacity>
-      </View>
+    <View style={styles.container} className=" bg-white dark:bg-gray-500">
+      <TouchableOpacity
+        className={
+          route?.name === RUTAS.HOME && "bg-gray-300 dark:bg-gray-600 "
+        }
+        onPress={() => navigation.navigate(RUTAS.HOME)}
+      >
+        <Image style={styles.img} source={icon.home} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        className={
+          route?.name === RUTAS.ENTRENATE && "bg-gray-300 dark:bg-gray-600 "
+        }
+      >
+        <Image style={styles.img} source={icon.entrenador} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        className={
+          route?.name === RUTAS.HAZLO && "bg-gray-300 dark:bg-gray-600 "
+        }
+      >
+        <Image style={styles.img} source={icon.prueba} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        className={
+          route?.name === RUTAS.RETATE && "bg-gray-300 dark:bg-gray-600 "
+        }
+      >
+        <Image style={styles.img} source={icon.graduado} />
+      </TouchableOpacity>
+      <TouchableOpacity
+        className={
+          (route?.name === RUTAS.LOGIN || route.name === RUTAS.REGISTER) &&
+          "bg-gray-300 dark:bg-gray-600 "
+        }
+        onPress={() => navigation.navigate(RUTAS.LOGIN)}
+      >
+        <Image style={styles.img} source={icon.usuario} />
+      </TouchableOpacity>
+    </View>
   );
 };
 
@@ -34,7 +54,6 @@ const styles = StyleSheet.create({
   container: {
     elevation: 20,
     paddingHorizontal: 8,
-    paddingTop: 4,
     flexDirection: "row",
     justifyContent: "space-between",
   },
