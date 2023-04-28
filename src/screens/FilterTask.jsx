90cf0ea954035f5c2507300e8ce970fab828e07b
andres0818 <c.andres.perez.h@gmail.com>
2023-04-27 23:21:30 -0500

    add stile for mode dark

--- src/screens/FilterTask.jsx
+++ src/screens/FilterTask.jsx
@@ -6,33 +6,36 @@ const FilterTask = () => {
   return (
     <View className="">
       <View className="flex  items-center px-8 ">
-        <Image className="absolute w-full h-full rounded-md" source={fondoFiltro} />
+        <Image
+          className="absolute w-full h-full rounded-md"
+          source={fondoFiltro}
+        />
         <Text className="text-white font-semibold text-base mt-5">
           Filtra el programa que deseas estudiar
         </Text>
         <View className="w-full px-8 mt-6 mb-8 ">
           <TextInput
             placeholder="Modalidad"
-            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 border rounded-t-lg"
+            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 dark:bg-gray-300 border rounded-t-lg"
           />
           <TextInput
             placeholder="Ciudad"
-            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 border"
+            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 dark:bg-gray-300 border"
           />
           <TextInput
             placeholder="¿Cuando presentarás el examen?"
-            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 border"
+            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 dark:bg-gray-300 border"
           />
           <TextInput
             placeholder="Jornada"
-            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 border"
+            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 dark:bg-gray-300 border"
           />
           <TextInput
             placeholder="Programa"
-            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 border rounded-b-lg"
+            className="bg-white w-full h-16 px-4 font-bold text-sm border-y-stone-500 dark:bg-gray-300 border rounded-b-lg"
           />
-          <TouchableOpacity className='flex items-center bg-green-400 rounded-lg px-8 h-12 mt-0.5 justify-center'>
-            <Text className='text-white font-bold text-base'>Buscar</Text>
+          <TouchableOpacity className="flex items-center bg-green-400 rounded-lg px-8 h-12 mt-0.5 justify-center">
+            <Text className="text-white font-bold text-base">Buscar</Text>
           </TouchableOpacity>
         </View>
       </View>
