3b6f8220aa25bf319808980c35d70f99f8032ce8
andres0818 <c.andres.perez.h@gmail.com>
2023-04-28 15:45:14 -0500

    clean code

--- src/components/ListVideos.jsx
+++ src/components/ListVideos.jsx
@@ -1,20 +1,16 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 import Videos from "./Videos";
+import HiddenBtn from "./HiddenBtn";
 
 const ListVideos = () => {
   return (
-    <View>
-      <Text className="text-center text-2xl font-bold mb-8 text-green-500">
-        Videos explicativos
-      </Text>
-      <View>
-        <Videos title="Ingles" />
-        <Videos title="Lenguaje" />
-        <Videos title="Matemáticas" />
-        <Videos title="Ciencias Sociales" />
-      </View>
-    </View>
+    <HiddenBtn>
+      <Videos title="Ingles" />
+      <Videos title="Lenguaje" />
+      <Videos title="Matemáticas" />
+      <Videos title="Ciencias Sociales" />
+    </HiddenBtn>
   );
 };
 
