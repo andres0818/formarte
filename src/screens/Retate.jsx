5ada535b09ae662faf4a5a811b95146b3b199788
andres0818 <c.andres.perez.h@gmail.com>
2023-04-28 00:41:59 -0500

    create component

--- src/screens/Retate.jsx
+++ src/screens/Retate.jsx
@@ -3,6 +3,8 @@ import { View } from "react-native";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
 import { KeyboardAvoidingScrollView } from "@tbnb/react-native-keyboard-sticky-avoiding-scroll-view";
+import FilterTask from "./FilterTask";
+import Ratings from "../components/Ratings";
 
 const Retate = ({ navigation, route }) => {
   return (
@@ -10,7 +12,13 @@ const Retate = ({ navigation, route }) => {
       <KeyboardAvoidingScrollView
         stickyFooter={<NavBar route={route} navigation={navigation} />}
       >
-        <Header titulo="Rétate" subtitulo="Expertos en Preicfes y Preuniversitarios"/>
+        <Header
+          titulo="Rétate"
+          subtitulo="Expertos en Preicfes y Preuniversitarios"
+        />
+        <FilterTask />
+        <View className='mb-10'/>
+        <Ratings />
       </KeyboardAvoidingScrollView>
     </View>
   );
